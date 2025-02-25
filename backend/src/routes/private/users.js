2201a8f355ce115497ff635fb4d799758fc35779
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-25 01:01:35 -0300

    fix: add error handling

--- backend/src/routes/private/users.js
+++ backend/src/routes/private/users.js
@@ -5,53 +5,72 @@ const router = express.Router()
 const prisma = new PrismaClient()
 
 //Adicionando usuários
-const users = []
 router.post('/', async (req, res) => {
 
-  const user = await prisma.tab_users.create({
-    data: {
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-      password: req.body.password
-    }
-  })
+  try {
+    const user = await prisma.tab_users.create({
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: req.body.password
+      }
+    })
 
-  res.status(201).json(user)
+    res.status(201).json(user)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Erro ao tentar cadastrar usuário, tente novamente' })
+  }
 })
 
 //Listando usuários
 router.get('/', async (req, res) => {
 
-  const users = await prisma.tab_users.findMany()
+  try {
+    const users = await prisma.tab_users.findMany()
 
-  res.status(200).json({ message: 'Listando os usuários:', users })
+    res.status(200).json({ message: 'Listando os usuários:', users })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Erro ao tentar consultar usuários, tente novamente' })
+  }
 })
 
 //Atualizando usuários
 router.put('/:id', async (req, res) => {
 
-  const user = await prisma.tab_users.update({
-    where: { id: req.params.id },
-    data: {
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-      password: req.body.password
-    }
-  })
+  try {
+    const user = await prisma.tab_users.update({
+      where: { id: req.params.id },
+      data: {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: req.body.password
+      }
+    })
 
-  res.status(200).json(user)
+    res.status(200).json(user)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Erro ao tentar atualizar usuário, tente novamente' })
+  }
 })
 
 //Deletando usuários
 router.delete('/:id', async (req, res) => {
 
-  const user = await prisma.tab_users.delete({
-    where: { id: req.params.id }
-  })
+  try {
+    const user = await prisma.tab_users.delete({
+      where: { id: req.params.id }
+    })
 
-  res.status(200).json({ message: 'Usuário deletado com sucesso', user })
+    res.status(200).json({ message: 'Usuário deletado com sucesso', user })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Erro ao tentar deletar usuário, tente novamente' })
+  }
 })
 
 export default router
