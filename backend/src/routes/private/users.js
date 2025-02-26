978236f4d33408738c5be1f65589d4727b3b2381
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-26 00:18:33 -0300

    feat: remodel tab_users

--- backend/src/routes/private/users.js
+++ backend/src/routes/private/users.js
@@ -13,7 +13,8 @@ router.post('/', async (req, res) => {
         name: req.body.name,
         email: req.body.email,
         phone: req.body.phone,
-        password: req.body.password
+        role: req.body.role,
+        isActive: req.body.isActive === "true" //Convertendo string para boolean
       }
     })
 
@@ -47,7 +48,8 @@ router.put('/:id', async (req, res) => {
         name: req.body.name,
         email: req.body.email,
         phone: req.body.phone,
-        password: req.body.password
+        role: req.body.role,
+        isActive: req.body.isActive === "true"
       }
     })
 
