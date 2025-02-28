7558b18bfa2513fe8367f00420e04bdf2912a7c8
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-02-28 00:16:35 -0300

    fix: update title on navigation

--- frontend/src/components/Sidebar/index.jsx
+++ frontend/src/components/Sidebar/index.jsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, useLocation } from 'react-router'
 import profile from '../../assets/profile.jpg'
 import { Home, User, Package, LogOut, ChevronRight } from "lucide-react"
 
 function Sidebar() {
+  // Hook para pegar a localização atual da URL
   const location = useLocation()
 
+  // Verifica se a rota ativa é a atual
   const isActive = (path) => location.pathname === path
+
+  // Estado que controla a funcionalidade toggle da sidebar, inicia fechada
   const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    // Insere o título respectivo da página atual
+    if (location.pathname === '/users') {
+      document.title = 'CRUD | Usuários'
+    } else if (location.pathname === '/products') {
+      document.title = 'CRUD | Produtos'
+    } else {
+      document.title = 'CRUD | Home'
+    }
+  }, [location]) // Executa sempre que a URL mudar
 
   return (
     <nav className={`fixed top-0 left-0 h-full bg-sky-950 z-10 transition-all ease-in-out transform whitespace-nowrap ${isOpen ? 'w-60 duration-200' : 'w-20 duration-900'}`}>
       <header className="flex items-center justify-center gap-3 !mt-4">
         <div className='w-1 h-4 bg-yellow-400'></div>
-        <h1 className={`${isOpen ? 'text-white uppercase font-bold tracking-widest' : 'hidden'}`}>CRUD System</h1><span className={isOpen ? 'hidden' : 'text-white uppercase font-bold tracking-widest'}>CS</span>
+        <h1 className={`${isOpen ? 'text-white uppercase font-bold tracking-widest' : 'hidden'}`}>CRUD System</h1>
+        {/* Abreviação do título, só fica visível quando a sidebar está fechada */}
+        <span className={isOpen ? 'hidden' : 'text-white uppercase font-bold tracking-widest'}>CS</span>
         <ChevronRight size={16} className={`absolute cursor-pointer w-5 h-5 text-black bg-yellow-400 rounded-full ${isOpen ? 'rotate-180 -right-1' : 'rotate-0 -right-3'}`} onClick={() => setIsOpen(!isOpen)} />
       </header>
       <div className='flex flex-col items-center gap-3 !mt-14'>
