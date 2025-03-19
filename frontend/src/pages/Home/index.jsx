1534d184420741596a6f0e1542af50c040b42594
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-03-19 01:21:10 -0300

    feat: create first chart with recharts lib

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import Sidebar from '../../components/Sidebar'
 import Main from '../../components/Main'
 import Cards from '../../components/Charts/Cards.jsx'
+import StockCategory from '../../components/Charts/StockCategory.jsx'
 
 function Home() {
 
@@ -10,6 +11,9 @@ function Home() {
       <Main>
         <h1 className='text-2xl text-gray-800 font-bold'>Home</h1>
         <Cards />
+        <div className='mt-20'>
+          <StockCategory />
+        </div>
       </Main>
     </div>
   )
